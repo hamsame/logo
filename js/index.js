a29626c33a2f040823e86d5fbde59fcffa724fb5
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2021-05-28 16:55:33 +0100

    moved 2 pics from the edge of the screen and added a click event on pages so it hides the navbar ul

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 document.querySelector('.hamburger').addEventListener('click', (e) => {
   document.querySelector('.nav-links').classList.toggle('shown')
 })
+
+document.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.remove('shown')
+  })
+})
+
+document.querySelectorAll('section').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.remove('shown')
+  })
+})
